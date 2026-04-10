62c10c904f9b40cbd36a36f29e15621618340c47
Fx <fx73000@yahoo.fr>
2026-04-10 02:00:16 +0200

    Reset routing to default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    pathMatch: 'full',
   }
 ];
